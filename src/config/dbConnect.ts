5c4653002196012a922c4c79d3f97cee8381aee9
Админ <admin@MacBook-Pro-Admin.local>
2024-01-21 10:23:10 +0200

    fix

--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -2,12 +2,12 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  port: process.env.DATABASEPORT,
-  password: process.env.DATABASEPASSWORD,
-  host: "localhost",
-  user: "postgres",
-  database: "halloffame",
-  // connectionString: process.env.POSTGRES_URL + '?sslmode=require',
+  // port: process.env.DATABASEPORT,
+  // password: process.env.DATABASEPASSWORD,
+  // host: "localhost",
+  // user: "postgres",
+  // database: "halloffame",
+  connectionString: process.env.POSTGRES_URL + '?sslmode=require',
 });
 
 module.exports = { pool };
